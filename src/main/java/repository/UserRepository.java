ad7199d7b0084c4294c3950941cc752b37c1c62d
maygul <88988446+maygul@users.noreply.github.com>
2023-03-01 08:04:29 +0300

    initial commit

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,115 @@
+package repository;
+
+import entity.User;
+import org.postgresql.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class UserRepository {
+
+    private static final Logger log = Logger.getLogger(UserRepository.class.getName());
+
+    private static final String jdbcURL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String jdbcUsername = "postgres";
+    private static final String jdbcPassword = "postgres";
+
+    public UserRepository() {
+        try {
+            DriverManager.registerDriver(new Driver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+        Connection conn = null;
+
+        conn = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        conn.setSchema("public");
+        return conn;
+    }
+
+    public void save(User user) throws SQLException {
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name,email,country) VALUES (?,?,?)");
+        ) {
+            log.info("user udate : " + user);
+
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public User findById(int id) throws SQLException {
+        User user = null;
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT name,email,country FROM users WHERE id=?");) {
+
+            log.info("findById: " + id);
+
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        }
+        return user;
+    }
+
+    public List<User> findAll() throws SQLException {
+
+        List<User> users = new ArrayList<>();
+
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users");
+                ResultSet rs = preparedStatement.executeQuery();
+        ) {
+            log.info("findAll");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add(new User(id, name, email, country));
+            }
+        }
+        return users;
+    }
+
+    public void deleteById(int id) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");) {
+
+            log.info("deleteById :" + id);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        }
+    }
+
+    public void update(User user) throws SQLException {
+        try (
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement("UPDATE users SET name=?,email=?,country=? WHERE id = ?");
+        ) {
+
+            log.info("update: " + user);
+
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setLong(4, user.getId());
+            statement.executeUpdate();
+        }
+    }
+}
---
+++ src/main/webapp/pages/user/user-list.jsp
@@ -0,0 +1,67 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>User Management Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark"
+         style="background-color: cornflowerblue">
+        <div>
+            <a href="https://www.javaguides.net" class="navbar-brand"> User
+                Management App </a>
+        </div>
+
+        <ul class="navbar-nav">
+            <li><a href="<%=request.getContextPath()%>/users/list"
+                   class="nav-link">Users</a></li>
+        </ul>
+    </nav>
+</header>
+<br>
+
+<div class="row">
+    <div class="container">
+        <h3 class="text-center">List of Users</h3>
+        <hr>
+        <div class="container text-right">
+            <a href="<%=request.getContextPath()%>/users/new" class="btn btn-primary">Create User</a>
+        </div>
+        <br>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Country</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <c:forEach var="user" items="${listUser}">
+
+                <tr>
+                    <td><c:out value="${user.id}"/></td>
+                    <td><c:out value="${user.name}"/></td>
+                    <td><c:out value="${user.email}"/></td>
+                    <td><c:out value="${user.country}"/></td>
+                    <td><a href="<%=request.getContextPath()%>/users/edit?id=<c:out value='${user.id}' />">Edit</a>
+                        &nbsp;&nbsp;&nbsp;&nbsp; <a
+                                href="<%=request.getContextPath()%>/users/delete?id=<c:out value='${user.id}' />">Delete</a>
+                    </td>
+                </tr>
+            </c:forEach>
+            <!-- } -->
+            </tbody>
+
+        </table>
+    </div>
+</div>
+</body>
+</html>
